9cec097d71929e6dcfe09dd3057a87fd4a877968
Tracy Reuther <tracyreuther@gmail.com>
2019-09-14 22:35:33 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(username: params[:username])
+    @user = User.find_by(:username => params[:username])
     if @user != nil && @user.password == params[:password]
-      session[:user_id] = @user_id
+      session[:user_id] = @user.id
       redirect to '/account'
     end
     erb :error
